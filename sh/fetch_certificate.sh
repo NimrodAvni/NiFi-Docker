fd78ff312305598452d325019d2da4f2ab6814b9
MaxKelada <nimrodavni7@gmail.com>
2020-03-26 13:09:51 +0200

    moved certificate generation to a separate file

---
+++ sh/fetch_certificate.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+if ! ( [ -z ${CA_SERVER} ] || [ -z ${CA_TOKEN} ] ); then
+
+    KEYSTORE_PATH=${KEYSTORE_PATH:=${NIFI_HOME}/conf/keystore.jks}
+    TRUSTSTORE_PATH=${TRUSTSTORE_PATH:=${NIFI_HOME}/conf/truststore.jks}
+    echo ${KEYSTORE_PATH}
+    cert_path=${NIFI_HOME}/conf/nifi-cert.pem
+    config_json_path=${NIFI_HOME}/conf/config.json
+
+    # If these files already exist, then there is no need to request for a new certificate
+    if ! ( [ -f ${KEYSTORE_PATH} ] && [ -f ${TRUSTSTORE_PATH} ] && [ -f ${cert_path} ] && [ -f ${config_json_path} ] ); then
+        subject_alternative_names=$(hostname -f),${HOSTNAME}
+
+        # Generate certificate
+        echo "Generating Certificate from CA ${CA_SERVER}"
+        ${NIFI_TOOLKIT_HOME}/bin/tls-toolkit.sh client -D ${NODE_IDENTITY:-"CN="${HOSTNAME}",OU=NIFI"} -c ${CA_SERVER} -t ${CA_TOKEN} -p ${CA_PORT:-8443} --subjectAlternativeNames ${subject_alternative_names}
+
+        # Move all files generated to their correct location
+        mv ./keystore.jks ${KEYSTORE_PATH}
+        mv ./truststore.jks ${TRUSTSTORE_PATH}
+        mv ./nifi-cert.pem ${cert_path}
+        mv ./config.json ${config_json_path}
+
+        # Set security values from config.json
+        export KEYSTORE_PATH
+        export TRUSTSTORE_PATH
+        export KEYSTORE_PASSWORD=$(cat ${config_json_path} | jq -r '.keyStorePassword')
+        export TRUSTSTORE_PASSWORD=$(cat ${config_json_path} | jq -r '.trustStorePassword')
+        export KEY_PASSWORD=$(cat ${config_json_path} | jq -r '.keyPassword')
+        export KEYSTORE_TYPE=$(cat ${config_json_path} | jq -r '.keyStoreType')
+        export TRUSTSTORE_TYPE=$(cat ${config_json_path} | jq -r '.trustStoreType')
+        echo 'fetch_certificate.sh $KEYSTORE_PATH'
+        echo ${KEYSTORE_PATH}
+    fi
+fi
--- sh/run.sh
+++ sh/run.sh
@@ -4,6 +4,7 @@ scripts_dir='/opt/nifi/scripts'
 
 [ -f "${scripts_dir}/common.sh" ] && . "${scripts_dir}/common.sh"
 
+sierra_scripts_dir=${scripts_dir}'/sierra'
 
 #replace nifi.properties with nifi.properties_base if it exists
 if [ -f ${NIFI_HOME}/conf/nifi.properties_base ]; then
@@ -21,36 +22,13 @@ fi
 # set load balance host
 prop_replace 'nifi.cluster.load.balance.host'                    "${NIFI_CLUSTER_LOAD_BALANCE_ADDRESS:-$HOSTNAME}"
 
-if ! ( [ -z ${CA_SERVER} ] || [ -z ${CA_TOKEN} ] ); then
-    echo 'hi mom'
-    ${CA_PORT:=8443}
-    ${KEYSTORE_PATH:=${NIFI_HOME}/conf/keystore.jks}
-    ${TRUSTSTORE_PATH:=${NIFI_HOME}/conf/truststore.jks}
-    echo 'keystore path'
-    echo ${KEYSTORE_PATH}
-    cert_path=${NIFI_HOME}/conf/nifi-cert.pem
-    config_json_path=${NIFI_HOME}/conf/config.json
+# we use source since we want all the exported variables from fetch_certificate.sh
+# to be set in the scope of our script, which does not work if we just use
+# ${sierra_scripts_dir}/fetch_certificate.sh
+# alternately we could use '.' instead of source
+source ${sierra_scripts_dir}/fetch_certificate.sh
 
-    #if these files already exist, then there is no need to request for a new certificate
-    if ! ( [ -f ${KEYSTORE_PATH} ] && [ -f ${TRUSTSTORE_PATH} ] && [ -f ${cert_path} ] && [ -f ${config_json_path} ] ); then
-        subject_alternative_names=$(hostname -f),${HOSTNAME}
-        ${NODE_IDENTITY:="CN="${HOSTNAME}",OU=NIFI"}
-        # generate certificate
-        ${NIFI_TOOLKIT_HOME}/bin/tls-toolkit.sh client -D ${NODE_IDENTITY} -c ${CA_SERVER} -t ${CA_TOKEN} -p ${CA_PORT} --subjectAlternativeNames ${subject_alternative_names}
-            mv ./keystore.jks ${KEYSTORE_PATH}
-            mv ./truststore.jks ${TRUSTSTORE_PATH}
-            mv ./nifi-cert.pem ${cert_path}
-            mv ./config.json ${config_json_path}
+echo 'run.sh $KEYSTORE_PATH'
+echo ${KEYSTORE_PATH}
 
-        # set security values from config.json
-        export KEYSTORE_PASSWORD=$(cat ${config_json_path} | jq -r '.keyStorePassword')
-        export TRUSTSTORE_PASSWORD=$(cat ${config_json_path} | jq -r '.trustStorePassword')
-        export KEY_PASSWORD=$(cat ${config_json_path} | jq -r '.keyPassword')
-        export KEYSTORE_TYPE=$(cat ${config_json_path} | jq -r '.keyStoreType')
-        export TRUSTSTORE_TYPE=$(cat ${config_json_path} | jq -r '.trustStoreType')
-
-    fi
-fi
-export KEYSTORE_PATH
-export TRUSTSTORE_PATH
 ${NIFI_BASE_DIR}/scripts/start.sh
