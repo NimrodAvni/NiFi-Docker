574b748e942219c33b8314f0daa62c29db301e52
MaxKelada <nimrodavni7@gmail.com>
2020-03-30 15:56:10 +0200

    fixed issues due to transition to full name command line arguments

--- sh/sierra_common.sh
+++ sh/sierra_common.sh
@@ -16,14 +16,14 @@ access_policy_property_xpath='//accessPolicyProvider/property'
 # 2 - file to look property in
 read_property () {
   target_file=${2:-${nifi_props_file}}
-  echo $(cat ${target_file} | grep $1 | cut --delimiter'=' -f2)
+  echo $(cat ${target_file} | grep $1 | cut --delimiter='=' -f2)
 }
 
 # 1 - property name
 # 2 - xpath
 # 3 - xml file
 read_xml_property () {
-    xmlstarlet select --template --match "$2[@name='$1']" --value . --nl $3
+    xmlstarlet select --template --match "$2[@name='$1']" --value-of . --nl $3
 }
 
 # 1 - xpath to property
@@ -37,7 +37,7 @@ edit_xml_property() {
   property_value=$3
   xml_file=$4
   if [ -n "${property_value}" ]; then
-    xmlstarlet edit --inplace --update "${property_xpath}[@name='${property_name}']" --value "${property_value}" ${xml_file}
+    xmlstarlet edit --inplace --update "${property_xpath}[@name='${property_name}']" -v "${property_value}" ${xml_file}
   fi
 }
 
@@ -46,7 +46,7 @@ edit_xml_property() {
 # 3 - property value
 # 4 - xml file
 add_xml_property () {
-    last_element_name=`xmlstarlet sel --template --match "$1[last()]" --value "@name" --nl $4`
+    last_element_name=`xmlstarlet sel --template --match "$1[last()]" -v "@name" --nl $4`
     xmlstarlet edit --inplace --append $1"[@name=\"${last_element_name}\"]" \
 		        --type 'elem' -n 'property' --value "$3" \
 			    --insert "$1[not(@name)]" \
