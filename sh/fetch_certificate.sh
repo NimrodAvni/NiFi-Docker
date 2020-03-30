16d3381182e9c44ead49183d131874aaed68df68
MaxKelada <nimrodavni7@gmail.com>
2020-03-30 15:01:34 +0200

    added quote to pervert space in strings to create buggy scenarios

--- sh/fetch_certificate.sh
+++ sh/fetch_certificate.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 if ! ( [ -z ${CA_SERVER} ] || [ -z ${CA_TOKEN} ] ); then
 
-    KEYSTORE_PATH=${KEYSTORE_PATH:=${NIFI_HOME}/conf/keystore.jks}
-    TRUSTSTORE_PATH=${TRUSTSTORE_PATH:=${NIFI_HOME}/conf/truststore.jks}
-    cert_path=${NIFI_HOME}/conf/nifi-cert.pem
-    config_json_path=${NIFI_HOME}/conf/config.json
+    KEYSTORE_PATH="${KEYSTORE_PATH:=${NIFI_HOME}/conf/keystore.jks}"
+    TRUSTSTORE_PATH="${TRUSTSTORE_PATH:=${NIFI_HOME}/conf/truststore.jks}"
+    cert_path="${NIFI_HOME}/conf/nifi-cert.pem"
+    config_json_path="${NIFI_HOME}/conf/config.json"
 
     # If these files already exist, then there is no need to request for a new certificate
     if ! ( [ -f ${KEYSTORE_PATH} ] && [ -f ${TRUSTSTORE_PATH} ] && [ -f ${cert_path} ] && [ -f ${config_json_path} ] ); then
-        subject_alternative_names=$(hostname -f),${HOSTNAME}
-        certificate_owner=${NODE_IDENTITY:-"CN="${HOSTNAME}", OU=NIFI"}
+        subject_alternative_names="$(hostname -f),${HOSTNAME}"
+        certificate_owner="${NODE_IDENTITY:-"CN="${HOSTNAME}", OU=NIFI"}"
         # Generate certificate
         echo "Generating Certificate from CA ${CA_SERVER} for ${certificate_owner}"
-        ${NIFI_TOOLKIT_HOME}/bin/tls-toolkit.sh client -D "${certificate_owner}" -c ${CA_SERVER} -t ${CA_TOKEN} -p ${CA_PORT:-8443} --subjectAlternativeNames ${subject_alternative_names}
+        ${NIFI_TOOLKIT_HOME}/bin/tls-toolkit.sh client -D "${certificate_owner}" -c "${CA_SERVER}" -t "${CA_TOKEN}" -p "${CA_PORT:-8443}" --subjectAlternativeNames "${subject_alternative_names}"
 
         # Move all files generated to their correct location
         mv ./keystore.jks ${KEYSTORE_PATH}
--- sh/init_authorizers.sh
+++ sh/init_authorizers.sh
@@ -11,4 +11,4 @@ node=${node:="CN=$(hostname), OU=NIFI"}
 edit_xml_property "${user_group_property_xpath}" "Initial User Identity 1" "${admin}" ${authorizers_file}
 edit_xml_property "${access_policy_property_xpath}" "Initial Admin Identity" "${admin}" ${authorizers_file}
 add_xml_property "${user_group_property_xpath}" "Initial User Identity $(uuidgen)" "${node}" ${authorizers_file}
-edit_xml_property "${access_policy_property_xpath}" "Node Identity 1" "${node}" "${authorizers_file}"
+edit_xml_property "${access_policy_property_xpath}" "Node Identity 1" "${node}" ${authorizers_file}
