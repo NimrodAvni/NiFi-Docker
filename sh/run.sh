d91637bc23c2dbe5cf7d56a79f095ff3e5da49f7
MaxKelada <nimrodavni7@gmail.com>
2020-05-12 20:21:47 +0200

    added feature to pull extra nars from git repository

--- sh/run.sh
+++ sh/run.sh
@@ -60,6 +60,8 @@ prop_replace 'nifi.cluster.load.balance.host'                    "${NIFI_CLUSTER
 # alternately we could use '.' instead of source
 source ${sierra_scripts_dir}/fetch_certificate.sh
 
+${sierra_scripts_dir}/pull_nars.sh
+
 ${sierra_scripts_dir}/set_authorizers.sh
 
 ${NIFI_BASE_DIR}/scripts/start.sh
